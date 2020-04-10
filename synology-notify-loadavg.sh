7d2454556c8d5917766303b9c48c6b96bdac6bd3
Calmar Bro <mac@calmar.ws>
2020-04-10 21:29:06 +0200

    typos

--- synology-notify-loadavg.sh
+++ synology-notify-loadavg.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Synology: wenn 5 avg_load minuten schwellenwert zu hoch, sende
-# eine Push Nachricht (oder was auch immer eingestellt ist, sms, email etc)
-# einstellungen -> benachrichtigungen -> erweitert -> 5minute-dings
+# Synology: wenn 5 avg_load Minuten Schwellenwert zu hoch, sende
+# eine Push-Nachricht (oder was auch immer eingestellt ist, sms, email etc)
+# Einstellungen -> Benachrichtigungen -> Erweitert -> 5minute-dings
 #
 #---[ Einstellungen ]---------------
 
-schwellenwert="1.7"              # vom 5minuten-load durchschnitt
+schwellenwert="1.7"              # vom 5minuten-load Durchschnitt
 pause_nach_push=300              # in Sekunden
 scan_alle_sekunden=30            # alle 30 Sekunden kontrollieren
-log_file="${BASH_SOURCE[0]}.log" # wo das script selber ist mit .log anhaengsel
+log_file="${BASH_SOURCE[0]}.log" # wo das Script selber ist mit .log anhaengsel
 
 #-----------------------------------
 
@@ -20,8 +20,8 @@ counter=0
 push_nachricht=0
 push_zeit=0
 
-# das script 'looped' eine stunde lang.. dann beendet es sich.
-# muss desshalb jede stunde neu gestartet werden  ueber cron ( 0 * * * *....)
+# das Script 'looped' eine Stunde lang.. dann beendet es sich.
+# muss desshalb jede Stunde neu gestartet werden z.B. ueber cron ( 0 * * * *....)
 
 while (( $counter < 3600/$scan_alle_sekunden))
 do
@@ -31,21 +31,21 @@ do
 
     zeit=$(date +%s)
 
-    # nur push-nachricht, wenn die letzte push_nachricht
-    # laenger als 300 sekunden her ist (5 minuten)
+    # nur Push-Nachricht, wenn die letzte Push-Nachricht
+    # laenger als "pause_nach_push" Sekunden her ist
 
     if (( $zeit - $push_zeit > $pause_nach_push )); then
 
       push_zeit=$(date +%s)
       /usr/syno/bin/synonotify PerfEvent_System_CPU_Avg_5
       echo >> "$log_file"
-      echo "[$(date)] ALERT: push nachricht gesendet" >> "$log_file"
+      echo "[$(date)] ALERT: Push-Nachricht gesendet" >> "$log_file"
     fi
   fi
-  sleep $scan_alle_sekunden
   printf "." >> "$log_file"
+  sleep $scan_alle_sekunden
 done
 
-echo '[done]' >> "$log_file"
+echo '[script beendet.]' >> "$log_file"
 
-# fertig..  stunde vorbei... ueber cron etc neustarten
+# fertig..  Stunde (3600 sec) vorbei... ueber cron etc neustarten
