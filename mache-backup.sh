6faaa7f69cfebccb4119b577bfcd73879486d792
Calmar Bro <mac@calmar.ws>
2020-05-21 17:13:53 +0200

    update

--- mache-backup.sh
+++ mache-backup.sh
@@ -2,14 +2,14 @@
 
 #-- EINSTELLUNGEN - Anzahl backups --
 
-BACKUP_ORDNER_ANZAHL=100
+BACKUP_ORDNER_ANZAHL=1000
 
 # backup.0 ist der neuste. backup.1 stufe aelter. backup.2 noch aelter etc
 
 #----------------------------------
 LOGFILE="/volume1/homes/oronso/bin/mache-backup.log"
 
-#echo "Versuche Platte einzuhaengen (umount)..." | tee >> $LOGFILE
+#echo "Versuche Platte einzuhaengen (umount)..." | tee -a $LOGFILE
 #/usr/syno/bin/synousbdisk -mount sdq
 
 #----------------------------------
@@ -19,12 +19,12 @@ LOGFILE="/volume1/homes/oronso/bin/mache-backup.log"
 if [[ ! $(mount | grep usbshare | grep fuseblk.ntfs) ]]
 then
     echo
-        echo '------[ ACHTUNG ] ----------------------------------------' | tee >> $LOGFILE
-        date | tee >> $LOGFILE
-        echo 'Die Backupplatte mit 2.8 TB ist nicht eingelegt' | tee >> $LOGFILE
-        echo 'Konnte nicht gefunden werden bei der "mount Befehlsausgabe"' | tee >> $LOGFILE
-        echo 'Der backup-prozess wird nicht ausgefuehrt!' | tee >> $LOGFILE
-        echo '----------------------------------------------------------' | tee >> $LOGFILE
+        echo '------[ ACHTUNG ] ----------------------------------------' | tee -a $LOGFILE
+        date | tee -a $LOGFILE
+        echo 'Die Backupplatte mit 2.8 TB ist nicht eingelegt' | tee -a $LOGFILE
+        echo 'Konnte nicht gefunden werden bei der "mount Befehlsausgabe"' | tee -a $LOGFILE
+        echo 'Der backup-prozess wird nicht ausgefuehrt!' | tee -a $LOGFILE
+        echo '----------------------------------------------------------' | tee -a $LOGFILE
 
         /usr/syno/bin/synonotify PerfEvent_System_CPU_Avg_1
         exit 0
@@ -47,20 +47,20 @@ fi
 
 if [ -z $DIR ]
 then
-        echo '------[ ACHTUNG ] ----------------------------------------' | tee >> $LOGFILE
-        date | tee >> $LOGFILE
-        echo 'Die Backupplatte mit 2.8 TB ist nicht eingelegt' | tee >> $LOGFILE
-        echo 'Konnte nicht gefunden werden via "df -h"' | tee >> $LOGFILE
-        echo 'Der backup-prozess wird nicht ausgefuehrt!' | tee >> $LOGFILE
-        echo '----------------------------------------------------------' | tee >> $LOGFILE
+        echo '------[ ACHTUNG ] ----------------------------------------' | tee -a $LOGFILE
+        date | tee -a $LOGFILE
+        echo 'Die Backupplatte mit 2.8 TB ist nicht eingelegt' | tee -a $LOGFILE
+        echo 'Konnte nicht gefunden werden via "df -h"' | tee -a $LOGFILE
+        echo 'Der backup-prozess wird nicht ausgefuehrt!' | tee -a $LOGFILE
+        echo '----------------------------------------------------------' | tee -a $LOGFILE
 
         /usr/syno/bin/synonotify PerfEvent_System_CPU_Avg_1
         exit 0
 fi
 
-echo "------[ BACKUP vom $(date) ] -------" | tee >> $LOGFILE
-echo "" | tee >> $LOGFILE
-echo "BackupPlatte ist gemounted an: [${DIR}]" | tee >> $LOGFILE
+echo "------[ BACKUP vom $(date) ] -------" | tee -a $LOGFILE
+echo "" | tee -a $LOGFILE
+echo "BackupPlatte ist gemounted an: [${DIR}]" | tee -a $LOGFILE
 
 # ------------------------------------------------------
 # Backup Ordner
@@ -71,7 +71,7 @@ i=$BACKUP_ORDNER_ANZAHL
 if [ -d "${DIR}/backup.0" ]
 then
   # erhoehe ab und mit 1 um 1
-  echo "Erhoehe alle backup-nummern um 1..." | tee >> $LOGFILE
+  echo "Erhoehe alle backup-nummern um 1..." | tee -a $LOGFILE
   while [ "$i" -gt 1 ]
   do
     o=$((i))
@@ -85,25 +85,29 @@ fi
 
 cp -al "${DIR}/backup.0" "${DIR}/backup.1"
 
-rsync -av --delete  /volume1/homes/oronso/  "${DIR}/backup.0/" | tee >> ${LOGFILE}
+# damit die Ordner das Datum haben wann der backup gemacht wurde
+
+rsync -av --delete  /volume1/homes/oronso/  "${DIR}/backup.0/" | tee -a ${LOGFILE}
+
+touch "${DIR}/backup.0"
 
 # ------------------------------------------------------
 # log....
 
-echo "" | tee >> ${LOGFILE}
-echo $(du -sh ${DIR}/backup.3) " (vor 3 Tagen)" | tee >> ${LOGFILE}
-echo $(du -sh ${DIR}/backup.2) " (vor 2 Tagen)" | tee  >> ${LOGFILE}
-echo $(du -sh ${DIR}/backup.1) " (vor 1-nem Tag)" | tee  >> ${LOGFILE}
-echo $(du -sh ${DIR}/backup.0) " (aktuelles Backup)" | tee >> ${LOGFILE}
-echo "" | tee >> ${LOGFILE}
-echo $(df -h | grep ${DIR}) " (Backupplatte Platz)" | tee >> ${LOGFILE}
+echo "" | tee -a ${LOGFILE}
+echo $(du -sh ${DIR}/backup.3) " (vor 3 Tagen)" | tee -a ${LOGFILE}
+echo $(du -sh ${DIR}/backup.2) " (vor 2 Tagen)" | tee  -a ${LOGFILE}
+echo $(du -sh ${DIR}/backup.1) " (vor 1-nem Tag)" | tee  -a ${LOGFILE}
+echo $(du -sh ${DIR}/backup.0) " (aktuelles Backup)" | tee -a ${LOGFILE}
+echo "" | tee -a ${LOGFILE}
+echo $(df -h | grep ${DIR}) " (Backupplatte Platz)" | tee -a ${LOGFILE}
 
-echo "" | tee >> ${LOGFILE}
+echo "" | tee -a ${LOGFILE}
 
-#cho "Versuche Platte auszuhaengen (umount)..." | tee >> $LOGFILE
+#cho "Versuche Platte auszuhaengen (umount)..." | tee -a $LOGFILE
 #/usr/syno/bin/synousbdisk -umount sdq
 
-date | tee >> $LOGFILE
-echo "------[ ... beendet ] -------" | tee >> $LOGFILE
-echo "" | tee >> ${LOGFILE}
+date | tee -a $LOGFILE
+echo "------[ ... beendet ] -------" | tee -a $LOGFILE
+echo "" | tee -a ${LOGFILE}
 
